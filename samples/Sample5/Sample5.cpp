6148aca618be276177501f3cefcfa6c3bb871d60
Leo Moll <leo.moll@yeasoft.com>
2000-08-24 17:27:41 +0000

    First step in new version

--- samples/Sample5/Sample5.cpp
+++ samples/Sample5/Sample5.cpp
@@ -25,6 +25,9 @@
  * HISTORY		: =============================================================
  * 
  * $Log$
+ * Revision 1.2  2000/05/30  11:17:36  leo
+ * Fixed some smaller bug
+ *
  * Revision 1.1  2000/05/30  10:59:15  leo
  * Initial revision
  *
@@ -34,15 +37,494 @@
 #include "Sample5.h"
 
 /*=============================================================================
+ * MACROS & DEFINES
+ *============================================================================*/
+// convenient macros for manipulating 64-bit integers to 32-bit integers
+#define LOWPART_DWORDLONG		((DWORDLONG) 0x00000000FFFFFFFF)
+#define HIGHPART_DWORDLONG		((DWORDLONG) 0xFFFFFFFF00000000)
+
+#define MAKEDWORDLONG(lo,hi)	( (DWORDLONG) ( ( (hi) << 32 ) | (lo) ) )
+#define LOWDWORD(x)				( (DWORD) ( (x) & LOWPART_DWORDLONG ) )
+#define HIDWORD(x)				( (DWORD) ( ( (x) & HIGHPART_DWORDLONG ) >> 32 ) )
+
+#define LOWPART_LONGLONG		((LONGLONG) 0x00000000FFFFFFFF)
+#define HIGHPART_LONGLONG		((LONGLONG) 0xFFFFFFFF00000000)
+
+#define LOWLONG(x)				( (DWORD) ( (x) & LOWPART_LONGLONG ) )
+#define HILONG(x)				( (LONG) ( ( (x) & HIGHPART_LONGLONG ) >> 32 ) )
+
+#define FLOPPYSIZE				1457664
+
+/*=============================================================================
  * INSTANTIATE
  *============================================================================*/
 FileSplitter			theApp;
 
 /*=============================================================================
+ * OUR COMMANDLINE PARSER
+ *============================================================================*/
+void CMyParser::ShowIntro ()
+{
+	// show credits
+	_tprintf (_T("\nThis is %s v%i.%i, the %s\n"), theApp.GetAppName (), theApp.GetMajorVersion (), theApp.GetMinorVersion (), theApp.GetAppDescription ());
+	_tprintf (_T("(c) 2000 YEAsoft Int'l. All rights reserved.\n\n"));
+}
+
+void CMyParser::ShowUsage (DWORDLONG dwCmd)
+{
+	YConApp	*pApp	= YlbGetApp ();
+
+	switch ( dwCmd ) {
+	case CMD_SPLIT:
+		_tprintf (_T("USAGE: %s split [-r] [-s <size>] [-p <number>] <sourcefile> [destpath]\n\n"), pApp->GetImageName ());
+		_tprintf (_T("       This command splits the sourcefile in one or more parts\n"));
+		_tprintf (_T("       If the destination path is not specified, the split parts are\n"));
+		_tprintf (_T("       stored in the source file directory.\n"));
+		_tprintf (_T("       The following options are available:\n\n"));
+		_tprintf (_T("       -r  Raw mode. The split parts contain no additional info.\n"));
+		_tprintf (_T("           By default the split parts contain additional info about the\n"));
+		_tprintf (_T("           original file and role of the split part.\n"));
+		_tprintf (_T("       -s  The size of the split parts specified in bytes. If the number\n"));
+		_tprintf (_T("           is followed by a K, this means that the number is interpreted\n"));
+		_tprintf (_T("           as kilobytes, if followed by an M, it is interpreted as megabytes.\n"));
+		_tprintf (_T("           The special number 1.44 means the size of a 1,44 Megabyte diskette.\n"));
+		_tprintf (_T("       -p  Part number. Creates only the part with the specified number.\n"));
+		_tprintf (_T("           It is also possible to specify more than one number separated\n"));
+		_tprintf (_T("           by commas. Also it is possible to specify a range (e.g. 4-7).\n"));
+		break;
+	case CMD_JOIN:
+		_tprintf (_T("USAGE: %s join <sourcefile pattern> [<sourcefile pattern> ...] <destpath>\n\n"), pApp->GetImageName ());
+		_tprintf (_T("USAGE: %s join -r <sourcefile> [<sourcefile> ...] <destfile>\n\n"), pApp->GetImageName ());
+		_tprintf (_T("       The first version of the command recreates the original file by\n"));
+		_tprintf (_T("       joining the specified parts. If parts are specified with wildcards, the\n"));
+		_tprintf (_T("       filenames are automatically expanded. The joined file will be\n"));
+		_tprintf (_T("       created in the specified destination directory. The file will also\n"));
+		_tprintf (_T("       be created if some part is missing. The missing range is than filled\n"));
+		_tprintf (_T("       with 0x00. If the destination is not complete, it's name will be\n"));
+		_tprintf (_T("       suffixed with the extension .partial. If such a partial file is found,\n"));
+		_tprintf (_T("       the specified parts are added to the file. If the file is complete,\n"));
+		_tprintf (_T("       it will be renamed to it's original name.\n\n"));
+		_tprintf (_T("       The second version simply concatenates the raw parts specified.\n"));
+		break;
+	case CMD_INFO:
+		_tprintf (_T("USAGE: %s info <filename>\n\n"), pApp->GetImageName ());
+		_tprintf (_T("       This command shows information about the specified file.\n"));
+		_tprintf (_T("       The file can be either a split part, or a partial joined file.\n"));
+		break;
+	default:
+		_tprintf (_T("USAGE: %s <cmd> [options] [params]\n\n"), pApp->GetImageName ());
+		_tprintf (_T("       where <cmd> can be:\n"));
+		_tprintf (_T("       split            Splits a file\n"));
+		_tprintf (_T("       join             Joins part files to a file\n"));
+		_tprintf (_T("       info             Shows information about a file\n"));
+		break;
+	}
+}
+
+void CMyParser::ShowExtro ()
+{
+	_tprintf (_T("\n"));
+}
+
+BOOL CMyParser::OnProcessParam (BOOL &bTerminate, YCmdLineParam &cliPar)
+{
+	typedef struct {
+		LPCTSTR	pszCmd;
+		DWORD	appCmd;
+	} CMDMATRIX;
+
+	static CMDMATRIX	cmCmds[] = {
+		{ _T("help"),		CMD_WANTSHELP },
+		{ _T("split"),		CMD_SPLIT },
+		{ _T("join"),		CMD_JOIN },
+		{ _T("info"),		CMD_INFO }
+	};
+
+	DWORD dwCmd = 0;
+	for ( DWORD dw = 0; dw < _countof (cmCmds); dw++ ) {
+		if ( !_tcsicmp (cmCmds[dw].pszCmd, cliPar.GetParam ()) ) {
+			dwCmd = cmCmds[dw].appCmd;
+			break;
+		} 
+	}
+	if ( dwCmd ) {
+		// command recognized. do not add to list.
+		SetCommandFlags (dwCmd);
+		return FALSE;
+	}
+	// must be a path/filename. add to list
+	cliPar.ResetAdditionalArgs ();
+	return TRUE;
+}
+
+BOOL CMyParser::OnProcessOption (BOOL &bTerminate, LPCTSTR &pszOptString, YCmdLineOption &cliOpt)
+{
+	switch ( cliOpt.GetOption () ) {
+	case _T('r'):
+		SetOptionFlags (OPT_RAWMODE);
+		cliOpt.SetMeaning (OPT_RAWMODE);
+		cliOpt.ResetAdditionalArgs ();
+		++pszOptString;
+		return TRUE;
+	case _T('s'):
+		SetOptionFlags (OPT_PARTSIZE);
+		cliOpt.SetMeaning (OPT_PARTSIZE);
+		cliOpt.SetParamCount (1);
+		++pszOptString;
+		return TRUE;
+	case _T('p'):
+		SetOptionFlags (OPT_PARTS);
+		cliOpt.SetMeaning (OPT_PARTS);
+		cliOpt.SetParamCount (1);
+		++pszOptString;
+		return TRUE;
+	}
+	SetOptionFlags (OPT_UNKNOWN);
+	return FALSE;
+}
+
+BOOL CMyParser::OnFinalCheck ()
+{
+	// no command or help wanted
+	if ( !GetCommands () || TestCommandFlags (CMD_WANTSHELP) ) {
+		SetCommandFlags (CMD_WANTSHELP);
+		return TRUE;
+	}
+	// only one command permitted
+	if ( TestCommandFlags (CMD_SPLIT|CMD_JOIN|CMD_INFO, FLG_OR) ) {
+		if ( !TestCommandFlags (CMD_SPLIT|CMD_JOIN|CMD_INFO, FLG_XOR) ) {
+			ShowIntro ();
+			_tprintf (_T("ERROR: more than one command specified\n\n"));
+			ShowUsage (GetCommands () & CMD_MASK);
+			ShowExtro ();
+			return FALSE;
+		}
+	}
+
+	// unknown option specified
+	if ( TestOptionFlags (OPT_UNKNOWN) ) {
+		ShowIntro ();
+		_tprintf (_T("ERROR: unknown option specified\n\n"));
+		ShowUsage (0);
+		ShowExtro ();
+		return FALSE;
+	}
+
+	// part number and split size only with split
+	if ( TestOptionFlags (OPT_PARTS|OPT_PARTSIZE, FLG_OR) && !TestCommandFlags (CMD_SPLIT) ) {
+		ShowIntro ();
+		_tprintf (_T("ERROR: option permitted only with 'split'\n\n"));
+		ShowUsage (GetCommands () & CMD_MASK);
+		ShowExtro ();
+		return FALSE;
+	}
+
+	// check parts
+	if ( TestOptionFlags (OPT_PARTS) ) {
+		if ( !FindOptionByMeaning (OPT_PARTS)->GetParamCount () ) {
+			ShowIntro ();
+			_tprintf (_T("ERROR: no parts specified with -p\n\n"));
+			ShowUsage (GetCommands () & CMD_MASK);
+			ShowExtro ();
+			return FALSE;
+		}
+	}
+
+	// check part size
+	if ( TestOptionFlags (OPT_PARTSIZE) ) {
+		if ( !FindOptionByMeaning (OPT_PARTSIZE)->GetParamCount () ) {
+			ShowIntro ();
+			_tprintf (_T("ERROR: no size specified with -p\n\n"));
+			ShowUsage (GetCommands () & CMD_MASK);
+			ShowExtro ();
+			return FALSE;
+		}
+	}
+
+	// check files
+	if ( !GetParamsCount () ) {
+		ShowIntro ();
+		_tprintf (_T("ERROR: no file specified\n\n"));
+		ShowUsage (GetCommands () & CMD_MASK);
+		ShowExtro ();
+		return FALSE;
+	}
+
+	// check files
+	if ( TestCommandFlags (CMD_JOIN, FLG_OR) ) {
+		if ( GetParamsCount () < 2 ) {
+			ShowIntro ();
+			_tprintf (_T("ERROR: no destination specified\n\n"));
+			ShowUsage (GetCommands () & CMD_MASK);
+			ShowExtro ();
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+}
+
+
+/*=============================================================================
  * OUR SAMPLE APPLICATION
  *============================================================================*/
-BOOL FileSplitter::InitInstance (YCommandLineInfo *cli)
+FileSplitter::FileSplitter () : YConApp (IDS_APP_TITLE)
 {
+	memset (&m_pi, 0, sizeof (m_pi));
+
+	m_pi.dwTag		= SPLOIN_HEADER;
+	m_pi.dwSize		= sizeof (m_pi);
+}
+
+void FileSplitter::Run ()
+{
+	CMyParser	cliParser;
+
+	if ( !cliParser.GetParserResult () ) {
+		// we had an error and we had already shown it
+		return;
+	}
+
+	cliParser.ShowIntro ();
+
+	if ( cliParser.TestCommandFlags (CMD_WANTSHELP) ) {
+		cliParser.ShowUsage (cliParser.GetCommands () & CMD_MASK);
+		cliParser.ShowExtro ();
+		return;
+	}
+
+	switch ( cliParser.GetCommands () ) {
+	case CMD_SPLIT:
+		SetReturnCode (Split (cliParser) ? (0) : (-1));
+		break;
+	case CMD_JOIN:
+		SetReturnCode (Join (cliParser) ? (0) : (-1));
+		break;
+	case CMD_INFO:
+		SetReturnCode (Info (cliParser) ? (0) : (-1));
+		break;
+	default:
+		SetReturnCode (-1);
+		break;
+	}
+
+	cliParser.ShowExtro ();
+	return;
+}
+
+BOOL FileSplitter::Info (CMyParser &cliParser)
+{
+	return FALSE;
+}
+
+BOOL FileSplitter::Split (CMyParser &cliParser)
+{
+	m_ysSrcName = cliParser.GetParam (0)->GetParam ();
+
+	if ( !m_ysSrcName.IsFile () ) {
+		ShowError (_T("File %s does not exist"), m_ysSrcName);
+		return FALSE;
+	}
+
+	if ( cliParser.GetParam (1) && !IsDir (cliParser.GetParam (1)->GetParam ()) ) {
+		ShowError (_T("Directory %s does not exist"), cliParser.GetParam (1)->GetParam ());
+		return FALSE;
+	}
+
+	if ( cliParser.GetParam (1) ) {
+		m_ysDestFormat = cliParser.GetParam (1)->GetParam ();
+		m_ysDestFormat.AppendBackslash (TRUE);
+	}
+	else {
+		m_ysDestFormat = m_ysSrcName;
+		m_ysDestFormat.StripFileName (TRUE);
+	}
+	m_ysDestFormat += m_ysSrcName.GetFileName ();
+	m_ysDestFormat.StripExtension ();
+	m_ysDestFormat += _T("-(%04.4i).");
+	m_ysDestFormat += (cliParser.TestOptionFlags (OPT_RAWMODE)) ? (_T(".spl")) : (m_ysSrcName.GetFileExtension ());
+
+	_tcscpy (m_pi.szFileName, m_ysSrcName.GetFileName ());
+
+	DWORD dwSizeHi, dwSizeLo;
+	if ( !IsFile (&m_pi.ftFileTime, &m_pi.dwAttributes, &dwSizeHi, &dwSizeLo, NULL, m_ysSrcName) ) {
+		ShowError (_T("Cannot get source file information"));
+		return FALSE;
+	}
+	m_pi.dwlFileSize = MAKEDWORDLONG (dwSizeLo, dwSizeHi);
+
+	YCmdLineOption *pPartSize = cliParser.FindOptionByMeaning (OPT_PARTSIZE);
+	if ( pPartSize && pPartSize->GetParamCount () ) {
+		LPCTSTR lpPtr = pPartSize->GetAt (ZERO);
+		if ( !_tcscmp (lpPtr, _T("1.44")) ) {
+			m_pi.dwPartSize = FLOPPYSIZE;
+		}
+		LPTSTR	lpEnd;
+		m_dwPartFileSize = _tcstoul (lpPtr, &lpEnd, 10);
+		if ( !m_dwPartFileSize ) {
+			ShowError (_T("Invalid part size '%s' specified."), lpPtr);
+			return FALSE;
+		}
+		switch ( *lpEnd ) {
+		case _T('k'):
+		case _T('K'):
+			if ( m_dwPartFileSize >= 0x00400000 ) {
+				ShowError (_T("Part size '%s' specified would exceed 4GB."), lpPtr);
+				return FALSE;
+			}
+			m_dwPartFileSize *= 1024;
+			break;
+		case _T('m'):
+		case _T('M'):
+			if ( m_dwPartFileSize >= 0x00001000 ) {
+				ShowError (_T("Part size '%s' specified would exceed 4GB."), lpPtr);
+				return FALSE;
+			}
+			m_dwPartFileSize *= (1024*1024);
+			break;
+		case 0:
+			break;
+		default:
+			ShowError (_T("Invalid part size '%s' specified."), lpPtr);
+			return FALSE;
+		}
+		if ( m_dwPartFileSize < (64*1024) ) {
+			ShowError (_T("Part size '%s' is less than 64KB."), lpPtr);
+			return FALSE;
+		}
+
+	}
+	else {
+		m_dwPartFileSize = FLOPPYSIZE;
+	}
+
+	if ( cliParser.TestOptionFlags (OPT_RAWMODE) ) {
+		return RawSplit (cliParser);
+	}
+
+	m_pi.dwPartSize	= m_dwPartFileSize - m_pi.dwSize;
+	m_pi.dwParts	= (DWORD) ((m_pi.dwlFileSize / m_pi.dwPartSize) + 1);
+
+	_tprintf (_T("Filename : %s\n"), m_pi.szFileName);
+	_tprintf (_T("Size     : %I64u Bytes\n"), m_pi.dwlFileSize);
+	_tprintf (_T("Part Size: %lu Bytes\n"), m_dwPartFileSize);
+	_tprintf (_T("Parts    : %lu\n\n"), m_pi.dwParts);
+
+	YFile	inFile;
+	YBuffer	ybBuffer;
+
+	if ( !ybBuffer.Alloc (128*1024) ) {
+		ShowError (_T("Cannot allocate memory"));
+		return FALSE;
+	}
+
+	if ( !inFile.Open (m_ysSrcName, YFile::modeRead|YFile::shareDenyWrite|YFile::cacheSequentialOptimized) ) {
+		ShowError (_T("Cannot open source file '%s'"), m_ysSrcName);
+		return FALSE;
+	}
+
+	YCmdLineOption *pParts = cliParser.FindOptionByMeaning (OPT_PARTS);
+	if ( !pParts ) {
+		// process all
+		for ( DWORD dwCnt = 0; dwCnt < m_pi.dwParts; dwCnt++ ) {
+			if ( !CreatePart (inFile, ybBuffer, dwCnt) ) {
+				return FALSE;
+			}
+		}
+	}
+	else {
+	}
+
+	return TRUE;
+}
+
+BOOL FileSplitter::Join (CMyParser &cliParser)
+{
+	return FALSE;
+}
+
+BOOL FileSplitter::RawSplit (CMyParser &cliParser)
+{
+	return FALSE;
+}
+
+BOOL FileSplitter::RawJoin (CMyParser &cliParser)
+{
+	return FALSE;
+}
+
+BOOL FileSplitter::CreatePart (YFile &inFile, YBuffer &workBuffer, DWORD dwPart)
+{
+	YCrc32			crc;
+	YFile			outFile;
+	YPathString		outFileName;
+
+	// position into the source file
+	DWORDLONG		dwlStartPos = ((DWORDLONG) dwPart * (DWORDLONG) m_pi.dwPartSize);
+	LONG			lStartPosHi = HILONG(dwlStartPos);
+	if ( !inFile.Seek (LOWDWORD(dwlStartPos), &lStartPosHi) ) {
+		ShowError (_T("Error positioning in source file (err: %d)"), ::GetLastError ());
+		return FALSE;
+	}
+
+	outFileName.Format (m_ysDestFormat, dwPart);
+	if ( !outFile.Open (outFileName, YFile::modeCreate|YFile::modeWrite|YFile::cacheSequentialOptimized) ) {
+		ShowError (_T("Error creating destination file '%s'"), outFileName);
+		return FALSE;
+	}
+
+	// reserve space for header
+	if ( !outFile.Write (workBuffer, sizeof (m_pi)) ) {
+		ShowError (_T("Error writing destination file '%s'"), outFileName);
+		return FALSE;
+	}
+
+	// write out the stuff
+	DWORD dwRest = m_pi.dwPartSize;
+
+	while ( dwRest ) {
+		if ( !inFile.Read (workBuffer, min (workBuffer.GetSize (), dwRest)) ) {
+			ShowError (_T("Error reading from source file (err: %d)"), ::GetLastError ());
+			return FALSE;
+		}
+		crc.UpdateCrc (workBuffer, workBuffer.GetSize ());
+		dwRest = (inFile.GetBytesRead () > dwRest) ? (0) : (dwRest - inFile.GetBytesRead ());
+		if ( !outFile.Write (workBuffer, inFile.GetBytesRead ()) ) {
+			ShowError (_T("Error writing to destination file (err: %d)"), ::GetLastError ());
+			return FALSE;
+		}
+	}
+
+	// position at beginning to write out header
+	if ( !outFile.Seek (0) ) {
+		ShowError (_T("Error positioning in destination file (err: %d)"), ::GetLastError ());
+		return FALSE;
+	}
+
+	m_pi.dwPart	= dwPart;
+	m_pi.dwCRC	= crc.GetCrc ();
+
+	if ( !outFile.Write (&m_pi, sizeof (m_pi)) ) {
+		ShowError (_T("Error writing to destination file (err: %d)"), ::GetLastError ());
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+void FileSplitter::ShowError (LPCTSTR pszFormat, ...)
+{
+	va_list va;
+
+	va_start (va, pszFormat);
+	_tprintf (_T("ERROR: "));
+	_vtprintf (pszFormat, va);
+	_tprintf (_T("\n"));
+	va_end (va);
+}
+
+
+#if (0)
 	_tprintf (_T("\nThis is %s, the %s\n"), GetAppName (), GetAppDescription ());
 	_tprintf (_T("(c) 2000 YEAsoft Int'l. All rights reserved.\n\n"));
 
@@ -154,24 +636,7 @@ BOOL FileSplitter::InitInstance (YCommandLineInfo *cli)
 	_tprintf (_T("Done.\n"));
 
 	return TRUE;
-}
-
-void FileSplitter::ShowError (LPCTSTR pszFormat, ...)
-{
-	va_list va;
-
-	va_start (va, pszFormat);
-	_tprintf (_T("\nERROR: "));
-	_vtprintf (pszFormat, va);
-	_tprintf (_T("\n"));
-	va_end (va);
-}
-
-void FileSplitter::ShowUsage ()
-{
-	_tprintf (_T("\nUSAGE: Split <filename> [chunk size]\n\n"));
-}
-
+#endif
 
 //
 // EoF
--- samples/Sample5/Sample5.h
+++ samples/Sample5/Sample5.h
@@ -25,6 +25,9 @@
  * HISTORY		: =============================================================
  * 
  * $Log$
+ * Revision 1.1  2000/05/30  10:59:37  leo
+ * Initial revision
+ *
  *============================================================================*/
 
 /*=============================================================================
@@ -33,18 +36,91 @@
 #include "SampleRes5.h"
 
 /*==================================================================================*
- * OUR SAMPLE APPLICATION
+ * DATA STRUCTURES
  *==================================================================================*/
-class FileSplitter : public YConApp
+#pragma pack (1)
+
+typedef struct _tagPARTINFO {
+	DWORD			dwTag;
+	DWORD			dwSize;
+	TCHAR			szFileName[MAX_PATH];
+	DWORD			dwAttributes;
+	FILETIME		ftFileTime;
+	DWORDLONG		dwlFileSize;
+	DWORD			dwPartSize;
+	DWORD			dwParts;
+	DWORD			dwPart;
+	DWORD			dwCRC;
+} PARTINFO;
+
+typedef struct _tagFOOTINFO {
+	DWORD			dwTag;
+	DWORD			dwSize;
+} FOOTINFO;
+
+#pragma pack ()
+
+#define SPLOIN_HEADER			'SLOI'
+#define SPLOIN_PARTFOOTER		'PART'
+
+/*=============================================================================
+ * OUR COMMANDLINE PARSER
+ *============================================================================*/
+// supported commands
+#define CMD_WANTSHELP			0x00000001
+
+#define CMD_SPLIT				0x00000002
+#define CMD_JOIN				0x00000004
+#define CMD_INFO				0x00000008
+
+#define CMD_MASK				0xfffffffe
+#define CMD_MASK_KNOWN			0x0000000e
+
+#define OPT_RAWMODE				0x00000001
+#define OPT_PARTSIZE			0x00000002
+#define OPT_PARTS				0x00000004
+#define OPT_UNKNOWN				0x80000000
+
+class CMyParser : public YCmdLineParser
 {
 public:
-	FileSplitter				() : YConApp (IDS_APP_TITLE) { }
+	CMyParser					() { Parse (); }
 
 public:
-	virtual BOOL				InitInstance			(YCommandLineInfo *cli);
+	// overridables
+	virtual void				ShowIntro				();
+	virtual void				ShowUsage				(DWORDLONG dwCmd);
+	virtual void				ShowExtro				();
+
+	virtual BOOL				OnProcessOption			(BOOL &bTerminate, LPCTSTR &pszOptString, YCmdLineOption &cliOpt);
+	virtual BOOL				OnProcessParam			(BOOL &bTerminate, YCmdLineParam &cliPar);
+	virtual BOOL				OnFinalCheck			();
+};
+
+/*==================================================================================*
+ * OUR SAMPLE APPLICATION
+ *==================================================================================*/
+class FileSplitter : public YConApp, public YFileManager
+{
+public:
+	FileSplitter				();
+
+public:
+	virtual void				Run						();
 
 protected:
+	BOOL						Info					(CMyParser &cliParser);
+	BOOL						Split					(CMyParser &cliParser);
+	BOOL						Join					(CMyParser &cliParser);
+	BOOL						RawSplit				(CMyParser &cliParser);
+	BOOL						RawJoin					(CMyParser &cliParser);
+	BOOL						CreatePart				(YFile &inFile, YBuffer &workBuffer, DWORD dwPart);
 	void						ShowError				(LPCTSTR pszFormat, ...);
-	void						ShowUsage				();
+
+	PARTINFO					m_pi;
+
+	DWORD						m_dwPartFileSize;
+	YPathString					m_ysSrcName;
+	YPathString					m_ysDestFormat;
 };
 
